c27bd056b140fc7c1d4c1893438588cfb635adef
Rayane k. <0117243@academico.ifmg.edu.br>
2026-09-18 04:05:19 +0000

    feat(lista): implementa Lcria, Ldestroi, Lvazia, Lcheia e Ltamanho

---
+++ lista/lista.c
@@ -0,0 +1,61 @@
+typedef struct no {
+    void *dado;
+    struct no *ant;
+    struct no *prox;
+} No;
+
+struct lista {
+    No *inicio;
+    No *fim;
+    int qtd;
+};
+
+ Lista Lcria(){
+     Lista l = (Lista) malloc(sizeof(struct lista));
+
+     if ( l != NULL ){
+        l->inicio = NULL;
+        l->fim = NULL;
+        l->qtd = 0;
+     }
+
+      return l; 
+ }
+
+  void Ldestroi( Lista l){
+    if ( l != NULL){
+        No* atual = l->inicio;
+        No* prox;
+
+        while (atual != NULL){
+            prox = atual->prox;
+            free(atual);
+            atual = prox;
+        }
+
+        free(l);
+    }
+  }
+
+  int Lvazia(Lista l){
+    if (l == NULL){
+        return 1;
+}
+    if (l->qtd == 0){
+        return 1;
+    }
+
+    return 0 ;
+  }
+
+  int Lcheia (Lista l){
+    return 0;
+  }
+
+  int Ltamanho (Lista l){
+     if (l == NULL){
+        return 0;
+    }
+
+    return l->qtd;
+  }
---
+++ lista/lista.h
@@ -0,0 +1,18 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+typedef struct no No;
+typedef struct lista *Lista;
+
+Lista Lcria();
+
+void Ldestroi(Lista l);
+
+int Lvazia(Lista l);
+
+int Lcheia(Lista l);
+
+int Ltamanho(Lista l);
+
+#endif
+
